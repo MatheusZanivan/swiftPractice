04abe1921d3175fcbfed4673f0717c806d51aa0c
MatheusZanivan <zanivanandrade@gmail.com>
2022-08-19 15:00:41 -0300

    Muita coisa

--- Gestures/Gestures/ViewController.swift
+++ Gestures/Gestures/ViewController.swift
@@ -13,11 +13,24 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         
-        let viewStatus = UIView(frame: CGRect(x: 0, y: 0, width: 325, height: 52))
-        viewStatus.backgroundColor = .gray
+        let minhaView = UIView(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
+        minhaView.backgroundColor = .green
+        minhaView.center = view.center
+        view.addSubview(minhaView)
+        
+        
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(gestureFired(_:)))
+//        por ser uiTApGestureREcognizer tem que falar o numero de toques que voce quer
+        gestureRecognizer.numberOfTapsRequired = 2
+        gestureRecognizer.numberOfTouchesRequired = 1
+        
+        minhaView.addGestureRecognizer(gestureRecognizer)
+        minhaView.isUserInteractionEnabled = true
         // Do any additional setup after loading the view.
     }
 
-
+    @objc func gestureFired(_ gesture: UITapGestureRecognizer){
+        print("gesture fired")
+    }
 }
 
---
+++ mapSwiftUiPractice/mapSwiftUiPractice/mapSwiftUiPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  mapSwiftUiPracticeApp.swift
+//  mapSwiftUiPractice
+//
+//  Created by Matheus Zanivan on 15/08/22.
+//
+
+import SwiftUI
+
+@main
+struct mapSwiftUiPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
